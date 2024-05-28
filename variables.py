7e9e02e8ba46375a39eeebf924c87f1283b48952
Dr. Ramkishor Sah <dr.ramkishorsah@Dr-ka-MacBook-Pro.local>
2024-05-28 18:38:18 +0530

    append with shorthand operations

--- variables.py
+++ variables.py
@@ -1,7 +1,17 @@
-x: int = 8;
+x: int = 8; # explicit typecasting
 
 print(x)
 
 x = "joker"
+# another way to initialize a string
+y: str = "joker"
 
-print(x)
+print(x,y)
+
+#we can also use if else for assignment
+
+num1: int = input("Enter a number : ")
+num2: int = input("Enter another number : ")
+
+largerNum = num1 if num1>num2 else num2
+print(f"The larger of the two number entered : {largerNum}")
