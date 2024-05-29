b76530ecaaf5b25bd4524c45a96bd55b1d1bb1cc
TarunKumarPro-Grammer <ramkishorsah73@gmail.com>
2024-05-29 22:02:37 +0530

    learned list in python

--- lessons/list.py
+++ lessons/list.py
@@ -8,3 +8,13 @@ if "tarun" in names:
     print("yes")
 else:
     print("no")
+    
+#same things applies to strings as well 
+#same way you index strings you can index list too!
+#we can make list on the spot
+
+#also known as list comprehension
+lst = [i for i in range(4)]
+print(lst)
+evenLst = [i for i in range(10) if i%2 == 0 ]
+print(evenLst)
